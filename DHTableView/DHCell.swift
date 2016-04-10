675eef56310b12f341a61d516ddb8ad8b47eacb6
Derrick Ho <derrickho328@gmail.com>
2016-04-10 13:41:11 -0400

    - minimum MVP data source drives the static content of the Tableview.

---
+++ DHTableView/DHCell.swift
@@ -0,0 +1,13 @@
+//
+//  DHCell.swift
+//  DHTableView
+//
+//  Created by Derrick  Ho on 4/9/16.
+//  Copyright © 2016 Derrick  Ho. All rights reserved.
+//
+
+import UIKit
+
+class DHCell: UITableViewCell {
+	@IBOutlet var wordLabel: UILabel!
+}
